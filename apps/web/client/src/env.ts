c21c40688f5d7618b0176968b34a6ad2b7478b31
Sarp <sarppehlivan444@gmail.com>
2026-02-01 15:32:05 +0300

    chore(client): change ./src/env.js to ./src/env.ts in next.config.js and add env types for env.ts file
    - Change "./src/env.js" to "./src/env.ts" in next.config.js
    - Add new environment variables related to Supabase to env.ts file

--- apps/web/client/src/env.ts
+++ apps/web/client/src/env.ts
@@ -19,6 +19,8 @@ export const env = createEnv({
      */
     client: {
         // NEXT_PUBLIC_CLIENTVAR: z.string(),
+        NEXT_PUBLIC_SUPABASE_URL: z.string(),
+        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string(),
     },
 
     /**
@@ -27,7 +29,8 @@ export const env = createEnv({
      */
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
-        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
