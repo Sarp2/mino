cf0aee73d3de5d190c1b2b6597dd8a4f42a5905c
Sarp <sarppehlivan444@gmail.com>
2026-02-07 18:25:22 +0300

    feat(client): add server action for authentication
    - Add login function to sign in with OAuth for Supabase
    - Add devLogin function to sign in with password for Supabase when we are in the development

---
+++ apps/web/client/src/app/login/actions.ts
@@ -0,0 +1,72 @@
+'use server';
+
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import { SEED_USER } from '@mino/db';
+
+import { env } from '@/env';
+import { createClient } from '@/utils/supabase/server';
+
+export enum SignInMethod {
+    GITHUB = 'github',
+    GOOGLE = 'google',
+    DEV = 'dev',
+}
+
+export async function login(
+    provider: SignInMethod.GITHUB | SignInMethod.GOOGLE,
+) {
+    const supabase = await createClient();
+
+    const origin = (await headers()).get('origin') ?? env.NEXT_PUBLIC_SITE_URL;
+    const redirectTo = `${origin}/auth/callback`;
+
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        redirect('/auth/redirect');
+    }
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+            redirectTo,
+        },
+    });
+
+    if (error) {
+        redirect('/error');
+    }
+
+    redirect(data.url);
+}
+
+export async function devLogin() {
+    if (env.NODE_ENV !== 'development') {
+        throw new Error('Dev login is only available in development mode');
+    }
+
+    const supabase = await createClient();
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+        redirect('/auth/redirect');
+    }
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email: SEED_USER.EMAIL,
+        password: SEED_USER.PASSWORD,
+    });
+
+    if (error) {
+        console.error('Error signing in with password:', error);
+        throw new Error(error.message);
+    }
+
+    redirect('/auth/redirect');
+}
