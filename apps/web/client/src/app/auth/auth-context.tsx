1ab50bb9b77865bb8d3a364e7075ec0024bfb621
Sarp <sarppehlivan444@gmail.com>
2026-02-12 08:37:26 +0300

    refactor(client): update useAuthContext
    - Delete lastSignInMethod from interface
    - Delete lastSignInMethod and setLastSignInMethod from state
    - Delete useEffect from AuthProvider
    - Delete entire lastSignInMethod functionality from AuthProvider

--- apps/web/client/src/app/auth/auth-context.tsx
+++ apps/web/client/src/app/auth/auth-context.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import localforage from 'localforage';
 
 import type { ReactNode } from 'react';
 
-import { LocalForageKeys } from '@/utils/constants';
-import { devLogin, login, SignInMethod } from '../login/actions';
+import { SignInMethod } from '@mino/models';
 
-const LAST_SIGN_IN_METHOD_KEY = 'lastSignInMethod';
+import { LocalForageKeys } from '@/utils/constants';
+import { devLogin, login } from '../login/actions';
 
 interface AuthContextType {
     signingInMethod: SignInMethod | null;
-    lastSignInMethod: SignInMethod | null;
     isAuthModelOpen: boolean;
     setIsAuthModelOpen: (open: boolean) => void;
     handleLogin: (
@@ -25,25 +24,10 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [lastSignInMethod, setLastSignInMethod] =
-        useState<SignInMethod | null>(null);
-
     const [signingInMethod, setSigningInMethod] = useState<SignInMethod | null>(
         null,
     );
     const [isAuthModelOpen, setIsAuthModelOpen] = useState<boolean>(false);
-
-    useEffect(() => {
-        const getLastSignInMethod = async () => {
-            const lastSignInMethod =
-                await localforage.getItem<SignInMethod | null>(
-                    LAST_SIGN_IN_METHOD_KEY,
-                );
-            setLastSignInMethod(lastSignInMethod);
-        };
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        getLastSignInMethod();
-    }, []);
 
     const handleLogin = async (
         method: SignInMethod.GITHUB | SignInMethod.GOOGLE,
@@ -57,7 +41,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
                     returnUrl,
                 );
             }
-            await localforage.setItem(LAST_SIGN_IN_METHOD_KEY, method);
             await login(method);
         } catch (error) {
             console.error('Error signing in with method: ', method, error);
@@ -88,7 +71,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         <AuthContext.Provider
             value={{
                 signingInMethod,
-                lastSignInMethod,
                 handleLogin,
                 handleDevLogin,
                 isAuthModelOpen,
