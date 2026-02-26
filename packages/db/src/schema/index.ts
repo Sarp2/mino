bce5d7844627be88c226402d467376092ea80d24
Sarp <sarppehlivan444@gmail.com>
2026-02-26 14:12:52 +0300

    feat(db): create table called projectCreateRequests and its type definitions
    - Create table called projectCreateRequests to inform UI via request to create the user's project
    - Create Postgres enum called projectCreateStatus to use in status field
    - Create projectCreateRequestInsertSchema Zod schema type
    - Create projectCreateRequestUpdateSchema Zod schema type
    - Create ProjectCreateRequest and NewProjectCreateRequest type definitions
    - Create helper type definitions in create.ts for projectCreateRequests fields
    - Add @mino/models as dependency to package.json

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,6 +1,7 @@
 export * from './canvas/canvas';
 export * from './frame/frames';
 export * from './project/branch';
+export * from './project/create';
 export * from './project/project';
 export * from './user/user';
 export * from './user/user-canvas';
--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export * from './project';
+export * from './project/create';
 export * from './branch';
