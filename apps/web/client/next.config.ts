ebc01afb81298ec2a721cdab387c923ccb441b99
Sarp <sarppehlivan444@gmail.com>
2026-03-18 09:28:47 +0300

    refactor(client): remove unused image remote patterns in next.config.ts

--- apps/web/client/next.config.ts
+++ apps/web/client/next.config.ts
@@ -2,14 +2,6 @@ import './src/env';
 
 /** @type {import("next").NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '*',
-            },
-        ],
-    },
 };
 
 export default config;
