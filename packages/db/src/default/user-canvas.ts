d8eea6f307d3ca0e1bfdea51055d1ffae1547aab
Sarp <sarppehlivan444@gmail.com>
2026-03-18 09:05:57 +0300

    refactor(db): prevent overrides from changing required linkage ids
    - Add Partial<Omit<UserCanvas, 'userId' | 'canvasId'>> = {} to prevent omitting "userId" and "canvasId"

--- packages/db/src/default/user-canvas.ts
+++ packages/db/src/default/user-canvas.ts
@@ -3,14 +3,14 @@ import type { UserCanvas } from '../schema';
 export const createDefaultUserCanvas = (
     userId: string,
     canvasId: string,
-    overrides: Partial<UserCanvas>,
+    overrides: Partial<Omit<UserCanvas, 'userId' | 'canvasId'>> = {},
 ): UserCanvas => {
     return {
+        ...overrides,
         userId,
         canvasId,
         scale: '0.7',
         x: '175',
         y: '100',
-        ...overrides,
     };
 };
