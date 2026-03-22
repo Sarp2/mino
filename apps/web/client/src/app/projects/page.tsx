25c3cca9b54da2f403ac3aa49973a2fd28a17f93
Sarp <sarppehlivan444@gmail.com>
2026-03-22 09:32:23 +0300

    feat(client): add prefetch in /projects/page.tsx for getting user

--- apps/web/client/src/app/projects/page.tsx
+++ apps/web/client/src/app/projects/page.tsx
@@ -2,6 +2,7 @@ import { api, HydrateClient } from '@/trpc/server';
 import { ProjectsContent } from './_components/projects-content';
 
 export default async function ProjectsPage() {
+    void api.user.get.prefetch();
     void api.project.list.prefetch();
 
     return (
